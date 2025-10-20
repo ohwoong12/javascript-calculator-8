e9e5b10cd782014035da1a7f7babb6ed0425f631
OH WOONG KWON <oungtom1212@naver.com>
2025-10-20 20:21:29 +0900

    feat: 기본 구분자와 커스텀 구분자 구분하는 함수 구현
    - if문으로 입력받은 값의 시작점이 "//"인지 구분
    - 결과에 따라 커스텀 구분자 처리 로직 분류
    - 구분하여 계산 후에 각각 split 메서드로 구분자를 기준으로 분류한 배열을 반환

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ class App {
   async run() {
     try {
       const input = await this.enterInput();
+
+      const splitValues = this.splitInputValue(input);
     } catch (error) {
       throw new Error(error);
     }
   }
   async enterInput() {
     this.input = await Console.readLineAsync(CACLULATE_MESSAGE.START);
+  }
+  splitInputValue() {
+    if (this.input.startsWith('//')) {
+      const customSepEndIndex = this.input.indexOf('\\n');
+      const customSeparator = this.input.slice(2, customSepEndIndex);
+      const toBeValue = this.input.slice(customSepEndIndex + 2);
+
+      return toBeValue.split(customSeparator);
+    }
+    return this.input.split(/,|:/);
+  }
   }
 }
 
