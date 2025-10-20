5d1625e051bd6c99b3c9ca554e9f1882c15ad5b4
OH WOONG KWON <oungtom1212@naver.com>
2025-10-20 20:05:22 +0900

    feat: 문자열 입력 받기
    - App.js에 enterInput 메서드 추가
    - 문자열 입력받는 기능 구현
    - mission-utils의 Console.readLineAsync를 사용하여 사용자 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import { CACLULATE_MESSAGE } from './Constants.js';
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await this.enterInput();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  async enterInput() {
+    this.input = await Console.readLineAsync(CACLULATE_MESSAGE.START);
+  }
 }
 
 export default App;
