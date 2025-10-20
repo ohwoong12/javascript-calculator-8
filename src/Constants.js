36ce3ee50aeb7a4d98657bd348eb84d4a8ae2e3e
OH WOONG KWON <oungtom1212@naver.com>
2025-10-20 19:40:42 +0900

    feat: 문자열 상수 객체 생성
    에러 메시지와 계산기 시작과 종료 메시지를 작성했습니다.
    상수만 따로 관리하여 개발 코드 가독성 향상

---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+export const ERROR_MESSAGE = {
+  INVALID_SEPARATOR: '[ERROR] 유효하지 않은 구분자입니다.',
+  INVALID_VALUE_ONLY_NUMBER: '[ERROR] 숫자 외에 다른 값이 포함되어 있습니다.',
+  INVALID_POSITIVE_NUMBER: '[ERROR] 음수가 포함되어 있습니다.',
+  INVALID_CHRACTER: '[ERROR] 커스텀 구분자는 문자만 가능합니다.',
+  INVALID_CORRECT_CUSTOMSEPARATOR: '[ERROR] 커스텀 구분자를 제대로 입력하세요.',
+  INVALID_OPERAND: '[ERROR] 피연산자를 제대로 입력해주세요.',
+};
+
+export const CACLULATE_MESSAGE = {
+  START: '덧셈할 문자열을 입력해 주세요.',
+  END: '결과 :',
+};
