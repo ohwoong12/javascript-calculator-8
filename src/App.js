cf30e6f91e6e0d3e96af53ba807bfdc500b96c8e
OH WOONG KWON <oungtom1212@naver.com>
2025-10-20 20:30:42 +0900

    feat: 빈 문자열 입력 시 예외 처리
    - 입력 받은 후 계산 과정을 거치기 전 명시적으로 빈 값인지 검사 후 계산하도록 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ class App {
     try {
       const input = await this.enterInput();
 
+      if (input === '') {
+        this.printResult(0);
+        return;
+      }
+
       const splitValues = this.splitInputValue(input);
       const result = this.calculate(splitValues);
       this.printResult(result);
@@ -15,6 +20,7 @@ class App {
 
   async enterInput() {
     this.input = await Console.readLineAsync(CACLULATE_MESSAGE.START);
+  }
 
   printResult() {
     Console.print(`${CACLULATE_MESSAGE.END} ${this.calculate()}`);
